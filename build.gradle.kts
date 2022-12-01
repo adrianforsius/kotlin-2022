9036f8cec099fb2fa996728e8e9b127be84f2b76
Adrian Forsius <adrian.forsius@schibsted.com>
2022-12-01 22:05:47 +0100

    Add day1

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,11 @@ repositories {
     mavenCentral()
 }
 
+dependencies {
+    implementation("org.junit.jupiter:junit-jupiter-engine:5.9.0")
+    implementation("org.assertj:assertj-core:3.23.1")
+}
+
 tasks {
     sourceSets {
         main {
--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,78 @@
+import org.assertj.core.api.Assertions.assertThat
+
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val lines = input
+        val splitAt = lines
+            .withIndex()
+            .filter { it.value == "" }
+            .map { it.index }
+
+
+        val split = splitAt
+            .windowed(2)
+            .map {
+                lines.subList(
+                    it[0] + 1,
+                    it[1],
+                )
+            }
+
+        val max = split.map {
+            it.map {
+                it.toInt()
+            }.sum()
+        }.max()
+        return max
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val lines = input
+        var splitAt = lines
+            .withIndex()
+            .filter { it.value == "" }
+            .map { it.index }
+        splitAt = listOf(0) + splitAt
+        splitAt = splitAt + listOf(lines.size)
+
+
+        val split = splitAt
+            .windowed(2)
+            .map {
+                lines.subList(
+                    it[0],
+                    it[1],
+                )
+            }
+
+        val sorted = split.map {
+            it.map{
+                it.toIntOrNull() ?: 0
+            }.sum()
+        }.sortedDescending()
+        return sorted.slice(0..2).sum()
     }
 
     // test if implementation meets criteria from the description, like:
+    // Test
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val output1 = part1(testInput)
+    println(output1)
+    assertThat(output1).isEqualTo(24000)
 
+    // Answer
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val outputAnswer1 = part1(input)
+    println(outputAnswer1)
+    assertThat(outputAnswer1).isEqualTo(67450)
+
+    // Test
+    val output2 = part2(testInput)
+    assertThat(output2).isEqualTo(45000)
+
+    // Answer
+    val outputAnswer2 = part2(input)
+    println(outputAnswer2)
+    assertThat(outputAnswer2).isEqualTo(199357)
 }
