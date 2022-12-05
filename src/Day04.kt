6c653ed467e027ce11fd6c1e712c32c769bbf732
Adrian Forsius <adrian.forsius@schibsted.com>
2022-12-05 08:52:21 +0100

    Day 04

--- src/Day04.kt
+++ src/Day04.kt
@@ -3,57 +3,11 @@ import org.assertj.core.api.Assertions.assertThat
 
 fun main() {
     fun part1(input: List<String>): Int {
-        val lines = input
-        var splitAt = lines
-            .withIndex()
-            .filter { it.value == "" }
-            .map { it.index }
-        splitAt = listOf(0) + splitAt
-        splitAt = splitAt + listOf(lines.size)
-
-
-        val split = splitAt
-            .windowed(2)
-            .map {
-                lines.subList(
-                    it[0] + 1,
-                    it[1],
-                )
-            }
-
-        val max = split.map {
-            it.map {
-                it.toInt()
-            }.sum()
-        }.max()
-        return max
+        return input.size
     }
 
     fun part2(input: List<String>): Int {
-        val lines = input
-        var splitAt = lines
-            .withIndex()
-            .filter { it.value == "" }
-            .map { it.index }
-        splitAt = listOf(0) + splitAt
-        splitAt = splitAt + listOf(lines.size)
-
-
-        val split = splitAt
-            .windowed(2)
-            .map {
-                lines.subList(
-                    it[0],
-                    it[1],
-                )
-            }
-
-        val sorted = split.map {
-            it.map{
-                it.toIntOrNull() ?: 0
-            }.sum()
-        }.sortedDescending()
-        return sorted.slice(0..2).sum()
+        return input.size
     }
 
     // test if implementation meets criteria from the description, like:
@@ -62,16 +16,16 @@ fun main() {
     val output1 = part1(testInput)
     assertThat(output1).isEqualTo(24000)
 
-    // Answer
-    val input = readInput("Day01")
-    val outputAnswer1 = part1(input)
-    assertThat(outputAnswer1).isEqualTo(67450)
-
-    // Test
-    val output2 = part2(testInput)
-    assertThat(output2).isEqualTo(45000)
-
-    // Answer
-    val outputAnswer2 = part2(input)
-    assertThat(outputAnswer2).isEqualTo(199357)
+//    // Answer
+//    val input = readInput("Day01")
+//    val outputAnswer1 = part1(input)
+//    assertThat(outputAnswer1).isEqualTo(67450)
+//
+//    // Test
+//    val output2 = part2(testInput)
+//    assertThat(output2).isEqualTo(45000)
+//
+//    // Answer
+//    val outputAnswer2 = part2(input)
+//    assertThat(outputAnswer2).isEqualTo(199357)
 }
